7940387574740a0c6cfceb209c409defe0ac516f
harshraj.sachan17@gmail.com <harshraj.sachan17@gmail.com>
2026-08-16 01:10:17 +0530

    Two-sum

---
+++ 1.two-sum.cpp
@@ -0,0 +1,19 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int sum = target - nums[i];
+            if(m.count(sum)) return{m[sum],i};
+            m[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
+// @leet end
