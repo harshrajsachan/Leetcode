f215195fcffc1fc9dcb97e401ce1218553d14355
harshraj.sachan17@gmail.com <harshraj.sachan17@gmail.com>
2026-08-18 18:45:00 +0530

    1812

---
+++ 1812.determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,15 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int a=coordinates[0]-'a';
+        int b=coordinates[1]-'1';
+        return (a+b)%2!=0? true:false; 
+    }
+};
+// @leet end
