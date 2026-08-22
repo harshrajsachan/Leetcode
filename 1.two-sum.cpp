e6c741504337491838fd0ca0788b7d759b7cbdf0
harshraj.sachan17@gmail.com <harshraj.sachan17@gmail.com>
2026-08-22 15:01:15 +0530

    harsh

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,7 +8,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<num.size();i++){
             int sum = target - nums[i];
             if(m.count(sum)) return {m[sum],i};
             m[nums[i]]=i;
