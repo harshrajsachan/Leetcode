390becbc5c1985be306da028178aea54c9da3399
harshraj.sachan17@gmail.com <harshraj.sachan17@gmail.com>
2026-09-22 15:27:43 +0530

    random

---
+++ 100.same-tree.cpp
@@ -0,0 +1,24 @@
+// @leet imports start
+#include <bits/stdc++.h>
+using namespace std;
+// @leet imports end
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+    }
+};
+// @leet end
